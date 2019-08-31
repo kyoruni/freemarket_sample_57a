9a1df8a1d558c7481c751fd69e5b2db478d5b0d0
yuto1234 <mjgda85200@yahoo.co.jp>
2019-08-31 11:57:59 +0900

    アイテムカードをトップページと商品詳細ページに表示（カテゴリー分け、画像など未完成部分有り）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,10 @@
 class ItemsController < ApplicationController
 
   def show
+    @items = Item.limit(6).order('created_at DESC')
   end
 
   def index
+    @items = Item.limit(4).order('created_at DESC')
   end
 end
