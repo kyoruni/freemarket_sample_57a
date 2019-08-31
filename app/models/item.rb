df6e6d451fadf7739e3f5063285a90d2ef50f445
yuto1234 <mjgda85200@yahoo.co.jp>
2019-08-31 14:28:26 +0900

    Itemモデルのアソシエーション定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  belongs_to :saler, class_name: 'User', foreign_key: 'saler_id'
+  belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id'
+  belongs_to :category
+  belongs_to :brand, optional: true
+  belongs_to :condition
+  belongs_to :delivery_day
+  belongs_to :delivery_way
+  belongs_to :size
 end
