cbc27b45594ba5b27e156430386e1c7eb19004fd
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2019-09-01 17:48:12 +0900

    step1.html設定

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,2 +1,19 @@
 class SignupController < ApplicationController
+
+  def step1
+    @user = User.new()
+    @user.build_address
+  end
+
+  def step2
+  end
+
+  def step3
+  end
+
+  def step4
+  end
+
+  def step5
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,16 @@ Rails.application.routes.draw do
   resources  :buys, only: [:index, :show]
   root 'hello#index'
 
-
+  resources :signup do
+    collection do
+      get 'step1' => 'signup#step1'
+      get 'step2' => 'signup#step2'
+      get 'step3' => 'signup#step3'
+      get 'step4' => 'signup#step4'
+      get 'step5' => 'signup#step5'# ここで、入力の全てが終了する
+      get 'done' # 登録完了後のページ
+    end
+  end
 
   get 'step1' => 'hello#step1'
   get 'step2' => 'hello#step2'
