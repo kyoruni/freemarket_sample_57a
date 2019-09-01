698d6253ce8ddec9e580773245802fe4714143d5
yuto1234 <mjgda85200@yahoo.co.jp>
2019-09-01 12:10:16 +0900

    仮データ の作成いじってみた

--- db/fixtures/development/d01_Item.rb
+++ db/fixtures/development/d01_Item.rb
@@ -8,4 +8,26 @@ Item.seed do |s|
   s.postage = "送料込み（出品者負担）"
   s.price   = 35000
   s.state   = ""
+  s.category_id = 300
+  s.brand_id = 1
+  s.condition_id = 1
+  s.delivery_day_id = 1
+  s.size_id = 1
+  s.region_id = 10
+end
+
+data_id += 1
+Item.seed do |s|
+  s.id      = data_id
+  s.name    = "Supreme x UNDERCOVER Reversible MA-1"
+  s.text    = "シュプリーム  × アンダーカバー 人気色 人気サイズ"
+  s.postage = "送料込み（出品者負担）"
+  s.price   = 40000
+  s.state   = ""
+  s.category_id = 280
+  s.brand_id = 2
+  s.condition_id = 6
+  s.delivery_day_id = 2
+  s.size_id = 1
+  s.region_id = 30
 end
