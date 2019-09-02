173d457166c2313dbf499d352fc9064e8b0b546c
miyuki-muneishi <kyoruni@gmail.com>
2019-09-02 14:42:56 +0900

    #44 カテゴリー取得処理の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,11 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @items = Item.limit(6).order('created_at DESC')
+    @categories = Category.order("id ASC")
   end
 
   def index
     @items = Item.limit(4).order('created_at DESC')
+    @categories = Category.order("id ASC")
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
   def show
+    @categories = Category.order("id ASC")
   end
 end
