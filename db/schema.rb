f9ca3b880fb14820d625b772c7b2f36ff948a043
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2019-09-03 12:21:56 +0900

    カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_02_010423) do
+ActiveRecord::Schema.define(version: 2019_09_03_031716) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "last_name", null: false
@@ -139,6 +139,10 @@ ActiveRecord::Schema.define(version: 2019_09_02_010423) do
     t.datetime "updated_at", null: false
     t.string "name", null: false
     t.date "birthday", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
