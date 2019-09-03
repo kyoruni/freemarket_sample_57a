d05166765cf162694a38dab24ee59b6740442936
yuto1234 <mjgda85200@yahoo.co.jp>
2019-09-03 14:35:22 +0900

    簡単にできるフォームから実装。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,35 @@ class ItemsController < ApplicationController
   def index
     @items = Item.limit(4).order('created_at DESC')
   end
+
+  def new
+    @item = Item.new
+    # collction_selectで選択肢を呼び出す記述
+    @root_category = Category.order("id ASC").limit(13)
+    @condition = Condition.order("id ASC")
+    @postage = Postage.order("id ASC")
+    @region = Region.order("id ASC")
+    @delivery_day = DeliveryDay.order("id ASC")
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save!
+    redirect_to root_path
+    # if @item.save
+    #   redirect_to root_path
+    # else
+    #   redirect_to new_item_path
+    # end
+  end
+
+  private
+
+  def item_params
+    # binding.pry
+    params.require(:item).permit(:name, :text, :category_id, :condition_id, :region_id, :postage_id, :delivery_day_id, :price )
+  end
 end
+
+
+# :category_id, :condition_id, :postage_id, :region_id, :delivery_day_id, 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,16 @@
 class Item < ApplicationRecord
 
   # active_hashを
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :saler, class_name: 'User', foreign_key: 'saler_id'
-  belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id'
+  # extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :saler, class_name: 'User', foreign_key: 'saler_id', optional: true
+  belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id', optional: true
   belongs_to :category
   belongs_to :brand, optional: true
   belongs_to :condition
   belongs_to :delivery_day
-  belongs_to :delivery_way
-  belongs_to :size
-  belongs_to_active_hash :region
+  # belongs_to :delivery_way
+  # belongs_to :size
+  belongs_to :region
+  belongs_to :postage
   has_many :images, dependent: :destroy
 end
