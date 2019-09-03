dbcbd6aae560386324ef9332adfa60327b95be4f
tajimata <tajimax0107@gmail.com>
2019-09-03 14:51:16 +0900

    ログアウトフロント実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources  :users, only: [:show]
+  resources  :users, only: [:show, :delete]
   resources  :items
   resources  :buys, only: [:index, :show]
   root 'hello#index'
-
-
 
   get 'step1' => 'hello#step1'
   get 'step2' => 'hello#step2'
@@ -13,4 +11,5 @@ Rails.application.routes.draw do
   get 'step4' => 'hello#step4'
   get 'step5' => 'hello#step5'
   get 'step6' => 'hello#step6'
+  get 'step7' => 'hello#step7'
 end
