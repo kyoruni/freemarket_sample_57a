bfeafc55e2a501436c72fdf87d2c35c19e607468
miyuki-muneishi <kyoruni@gmail.com>
2019-09-05 15:19:02 +0900

    #65 カテゴリー、ブランド別に表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,22 @@ class ItemsController < ApplicationController
   end
 
   def index
-    @items = Item.limit(4).order('created_at DESC')
+    ladies_categories    = Category.where('ancestry LIKE(?)', "1/%")
+    mens_categories      = Category.where('ancestry LIKE(?)', "2/%")
+    kids_categories      = Category.where('ancestry LIKE(?)', "3/%")
+    cosmetics_categories = Category.where('ancestry LIKE(?)', "7/%")
+    chanel_id            = Brand.find_by(name: "シャネル")
+    louisVuitton_id      = Brand.find_by(name: "ルイ ヴィトン")
+    supreme_id           = Brand.find_by(name: "シュプリーム")
+    nike_id              = Brand.find_by(name: "ナイキ")
+
+    @ladies_items       = Item.includes(:images).where(category_id: ladies_categories.ids).order('created_at DESC').limit(4)
+    @mens_items         = Item.includes(:images).where(category_id: mens_categories.ids).order('created_at DESC').limit(4)
+    @kids_items         = Item.includes(:images).where(category_id: kids_categories.ids).order('created_at DESC').limit(4)
+    @cosmetics_items    = Item.includes(:images).where(category_id: cosmetics_categories.ids).order('created_at DESC').limit(4)
+    @chanel_items       = Item.includes(:images).where(brand_id: chanel_id).order('created_at DESC').limit(4)
+    @louisVuitton_items = Item.includes(:images).where(brand_id: louisVuitton_id).order('created_at DESC').limit(4)
+    @supreme_items      = Item.includes(:images).where(brand_id: supreme_id).order('created_at DESC').limit(4)
+    @nike_items         = Item.includes(:images).where(brand_id: nike_id).order('created_at DESC').limit(4)
   end
 end
