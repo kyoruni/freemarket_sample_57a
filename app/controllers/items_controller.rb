3dadfa5a2261dd03b7fda7b094ce62d7aa606dc1
miyuki-muneishi <kyoruni@gmail.com>
2019-09-05 18:43:33 +0900

    #65 共通の処理をまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,13 +15,13 @@ class ItemsController < ApplicationController
     supreme_id           = Brand.find_by(name: "シュプリーム")
     nike_id              = Brand.find_by(name: "ナイキ")
 
-    @ladies_items       = Item.includes(:images).where(category_id: ladies_categories.ids).order('created_at DESC').limit(4)
-    @mens_items         = Item.includes(:images).where(category_id: mens_categories.ids).order('created_at DESC').limit(4)
-    @kids_items         = Item.includes(:images).where(category_id: kids_categories.ids).order('created_at DESC').limit(4)
-    @cosmetics_items    = Item.includes(:images).where(category_id: cosmetics_categories.ids).order('created_at DESC').limit(4)
-    @chanel_items       = Item.includes(:images).where(brand_id: chanel_id).order('created_at DESC').limit(4)
-    @louisVuitton_items = Item.includes(:images).where(brand_id: louisVuitton_id).order('created_at DESC').limit(4)
-    @supreme_items      = Item.includes(:images).where(brand_id: supreme_id).order('created_at DESC').limit(4)
-    @nike_items         = Item.includes(:images).where(brand_id: nike_id).order('created_at DESC').limit(4)
+    @ladies_items       = Item.category_items(ladies_categories).recent(4)
+    @mens_items         = Item.category_items(mens_categories).recent(4)
+    @kids_items         = Item.category_items(kids_categories).recent(4)
+    @cosmetics_items    = Item.category_items(cosmetics_categories).recent(4)
+    @chanel_items       = Item.brand_items(chanel_id).recent(4)
+    @louisVuitton_items = Item.brand_items(louisVuitton_id).recent(4)
+    @supreme_items      = Item.brand_items(supreme_id).recent(4)
+    @nike_items         = Item.brand_items(nike_id).recent(4)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -12,4 +12,13 @@ class Item < ApplicationRecord
   belongs_to :size
   belongs_to_active_hash :region
   has_many :images, dependent: :destroy
+
+  # 最新のデータを、指定した件数取得
+  scope :recent, -> count { order('created_at DESC').limit(count) }
+
+  # 指定したカテゴリーのデータを取得
+  scope :category_items, -> categories { includes(:images).where(category_id: categories.ids ) }
+
+  # 指定したブランドのデータを取得
+  scope :brand_items, -> brand_id { includes(:images).where(brand_id: brand_id ) }
 end
