6ba13b1f2054d800854f0be7a41352b3b576be6c
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2019-09-05 11:31:58 +0900

    html修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one :address
-  accepts_nested_attributes_for :address
   has_one :shipping_address
   accepts_nested_attributes_for :shipping_address
   has_one :credit
