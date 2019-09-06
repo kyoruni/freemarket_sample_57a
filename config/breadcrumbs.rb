10315e83370ebac0ad62104bfe07ba04e5c3dd86
tajimata <tajimax0107@gmail.com>
2019-09-06 14:31:13 +0900

    view修正（仮）

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,3 +1,31 @@
+# crumb :root do
+#   link "Home", root_path
+# end
+
+# # user#index
+# crumb :users do
+#   link "Users", users_path
+#   parent :root
+# end
+
+# # user#show
+# crumb :show_user do |user|
+#   link "本人情報の登録", user
+#   parent :users
+# end
+
+# # user#edit
+# crumb :edit_user do |user|
+#   link "Edit #{user.name}", edit_user_path(user)
+#   parent :show_user, user
+# end
+
+# # user#new
+# crumb :new_user do |user|
+#   link "New User", new_user_path(user)
+#   parent :users
+# end
+
 crumb :root do
   link "メルカリ", root_path
 end
@@ -9,10 +37,58 @@ crumb :mypage do
 end
 
 # 本人情報の登録
-crumb :personal_info_regist do
-  link "本人情報の登録", '/step6'
+crumb :personal_info_regist do |user|
+  link "本人情報の登録", step6_path(current_user)
   parent :mypage
 end
+
+# ログアウち
+crumb :logout do |user|
+  link "ログアウト", step7_path(current_user)
+  parent :mypage
+end
+
+# クレジットカード登録
+crumb :card_registration do |user|
+  link "支払い方法", card_registration_path(current_user)
+  parent :mypage
+end
+
+crumb :tag do |tag|
+  if current_page?(step6_path)
+    show_post_tag_id = TagMap.find_by(post_id: params[:id]).tag_id
+    show_post_tag = Tag.find_by(id: show_post_tag_id)
+    link show_post_tag.name
+  else
+    tag_page = Tag.find_by(id: params[:id])
+    link tag_page.name
+  end
+  parent :root
+end
+
+
+
+# # user#show
+# crumb :show_user do |user|
+#   link user.name, user
+#   parent :users
+# end
+
+# # user#edit
+# crumb :edit_user do |user|
+#   link "Edit #{user.name}", edit_user_path(user)
+#   parent :show_user, user
+# end
+
+# # user#new
+# crumb :new_user do
+#   link "New User", new_user_path
+#   parent :users
+# end
+
+
+
+
 
 # # プロフィール編集
 # crumb :mypage do
@@ -43,3 +119,42 @@ end
 # files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
 # folder are loaded and reloaded automatically when you change them, just like
 # this file (`config/breadcrumbs.rb`).
+
+
+
+
+
+
+
+# crumb :root do
+#   link "Home", root_path
+# end
+
+# crumb :tag do |tag|
+#   if current_page?(:controller => 'post', :action => 'show')
+#     show_post_tag_id = TagMap.find_by(post_id: params[:id]).tag_id
+#     show_post_tag = Tag.find_by(id: show_post_tag_id)
+#     link show_post_tag.name
+#   else
+#     tag_page = Tag.find_by(id: params[:id])
+#     link tag_page.name
+#   end
+#   parent :root
+# end
+
+# crumb :show_title do |show_title|
+#   show_title = Post.find_by(id: params[:id])
+#   link show_title.title
+#   parent :tag
+# end
+
+
+# if value > 0 then
+#   puts "値は正です"
+# elsif value < 0 then
+#   puts "値は負です"
+# else
+#   puts "値は0です"
+# end
+
+
