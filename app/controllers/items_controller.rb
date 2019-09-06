04bb8d4bdc52abb69911c1d07edd9fd06a28f147
yuto1234 <mjgda85200@yahoo.co.jp>
2019-09-06 14:34:05 +0900

    renderに変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,7 +42,7 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
-      redirect_to new_item_path
+      render action: :new
     end
   end
 
