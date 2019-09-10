a29c8698d37158926450e5cc25ba229cceda7d50
miyuki-muneishi <kyoruni@gmail.com>
2019-09-10 14:54:28 +0900

    #44 カテゴリーごとのページを作成

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,10 @@
 class CategoriesController < ApplicationController
+  before_action :set_category_list, only: [:show]
+  def show
+
+    @category = Category.find(params[:id])
+    @category_items = Item.includes(:images).where(category_id: @category.id ).order('created_at DESC')
+    # ランダムで子カテゴリーを9件＆孫カテゴリーを2件取得
+    @category_list = @category.children.order("RAND()").limit(9) | @category.indirects.order("RAND()").limit(2)
+  end
 end
