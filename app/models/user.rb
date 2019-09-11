77b0093c4259a4a2619540cb13150897bde50270
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2019-09-11 16:28:28 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,28 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_one :shipping_address
   accepts_nested_attributes_for :shipping_address
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # step1入力項目
+  validates :name,                    presence: true, length: {maximum: 20}
+  validates :email,                   presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password,                presence: true, length: {minimum: 6, maximum: 128}
+  validates :password_confirmation,   presence: true, length: {minimum: 6, maximum: 128}
+  validates :last_name,               presence: true
+  validates :first_name,              presence: true
+  validates :last_name_kana,          presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
+  validates :first_name_kana,         presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
+  validates :birth_year,              presence: true
+  validates :birth_month,             presence: true
+  validates :birth_day,               presence: true
+  # step2入力項目
+  validates :phone_number,            presence: true
+  # step3入力項目
+  validates :last_name,               presence: true
+  validates :first_name,              presence: true
+  validates :last_name_kana,          presence: true
+  validates :first_name_kana,         presence: true
+  validates :postal_code,             presence: true
+  validates :address,                 presence: true
+
 end
