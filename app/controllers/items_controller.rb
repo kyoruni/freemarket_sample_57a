2ce504cc9b3f56b66607d80855706ce23aed1963
YutoAraki <mjgda85200@yahoo.co.jp>
2019-09-11 16:01:07 +0900

    孫カテゴリー保存できるように設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,15 +31,28 @@ class ItemsController < ApplicationController
     10.times{@item.images.build}
 
     # collction_selectで選択肢を呼び出す記述
-    @root_category = Category.limit(13)
+    @category_parent_array = Category.where(ancestry: nil)
     @condition = Condition.all
     @postage = Postage.all
     @region = Region.all
     @delivery_day = DeliveryDay.all
   end
 
+  # 親カテゴリーが選択された後に動くアクション
+  def get_category_children
+    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
+    @category_children = Category.find("#{params[:parent_id]}").children
+  end
+
+  # 子カテゴリーが選択された後に動くアクション
+  def get_category_grandchildren
+    #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
+  end
+
   def create
     @item = Item.new(item_params)
+    @parents = Category.where(ancestry: nil)
     if @item.save
       redirect_to root_path
     else
@@ -50,6 +63,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
+    binding.pry
     params.require(:item).permit(:name, :text, :category_id, :condition_id, :region_id, :postage_id, :delivery_day_id, :price, images_attributes: [:id, :image] )
   end
 end
