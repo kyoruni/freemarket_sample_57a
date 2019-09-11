1d9653167d433f9d264d5b9f87d3128cf34440ca
tajimata <tajimax0107@gmail.com>
2019-09-11 16:23:36 +0900

    sms認証用のリンクを貼る

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,31 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,:omniauthable
+         :recoverable, :rememberable, :validatable,:omniauthable,
+         omniauth_providers: %i[facebook google_oauth2]
+
   has_one :shipping_address
   accepts_nested_attributes_for :shipping_address
+
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        nickname:     auth.info.name,
+        email:    auth.info.email,
+        password: Devise.friendly_token[0, 20]
+      )
+    end
+
+    user
+  end
+
+  private
+
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
+  end
 end
