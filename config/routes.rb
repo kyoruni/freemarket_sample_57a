99a6d11538e7c8a0075b294d4f7e82dcb9b014f6
YutoAraki <mjgda85200@yahoo.co.jp>
2019-09-13 12:50:18 +0900

    #90 カード作成画面を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  get 'card/new'
-  get 'card/show'
+
+  # payjp
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
+  
   devise_for :users, controllers:{
     sessions: 'users/sessions'
   }
