048d8c2029489702af288c5383f935220db32cea
YutoAraki <mjgda85200@yahoo.co.jp>
2019-09-13 16:41:01 +0900

    #90 購入完了画面を編集

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -20,14 +20,16 @@ class PurchaseController < ApplicationController
   end
 
   def pay
+    @item = Item.find(params[:item_id])
     card = Card.where(user_id: current_user.id).first
     Payjp.api_key = "sk_test_6da2ad963bd122a05e78c8b7"
     Payjp::Charge.create(
-    :amount => 13500, #支払金額を入力（itemテーブル等に紐づけても良い）
+    :amount => @item.price, #支払金額を入力（itemテーブル等に紐づけても良い）
     :customer => card.customer_id, #顧客ID
     :currency => 'jpy', #日本円
-  )
-  redirect_to action: 'done' #完了画面に移動
+    )
+    @item.update(buyer_id: current_user.id)
+    redirect_to action: 'done' #完了画面に移動
   end
 
 end
