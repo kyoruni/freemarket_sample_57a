8d721c0be4b8dd75f06f2d4f0f2984600ab3c91a
miyuki-muneishi <kyoruni@gmail.com>
2019-09-14 18:57:44 +0900

    #100 コントローラーとルーティングを作成

---
+++ app/controllers/brands_controller.rb
@@ -0,0 +1,5 @@
+class BrandsController < ApplicationController
+  before_action :set_category_list, only: [:show]
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,7 @@ Rails.application.routes.draw do
   end
 
   resources  :categories, only: [:show]
+  resources  :brands,     only: [:show]
   root 'items#index'
 
 
