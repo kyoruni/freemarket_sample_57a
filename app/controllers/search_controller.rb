1bd720e65b9a75737d76d5918bc95a021fd834f9
miyuki-muneishi <kyoruni@gmail.com>
2019-09-14 17:10:56 +0900

    #95 取得できなかった場合、新着商品を取得

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,9 @@
 class SearchController < ApplicationController
   before_action :set_category_list, only: [:index]
   def index
-    @items = Item.all
+    # 検索ワードが入力されていたら、検索
+    @items     = Item.where('name LIKE(?)', "%#{params[:keyword]}%").order("created_at DESC") unless params[:keyword].empty?
+    # 検索結果がなければ、新着商品を取得
+    @new_items = Item.order("created_at DESC") unless @items.present?
   end
 end
