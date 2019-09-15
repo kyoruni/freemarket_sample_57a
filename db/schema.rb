48d06189dc5f62ad3c9c7c5a097afc41b0945a84
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2019-09-15 10:41:06 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_09_13_094810) do
-
-ActiveRecord::Schema.define(version: 2019_09_13_033753) do
-
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "last_name", null: false
@@ -131,7 +127,7 @@ ActiveRecord::Schema.define(version: 2019_09_13_033753) do
 
   create_table "shipping_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code"
-    t.string "address", default: "", null: false
+    t.string "address", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
