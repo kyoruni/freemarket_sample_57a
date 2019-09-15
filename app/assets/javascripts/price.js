b65d2c24ee8bbe7a5619e3704b721c2e87262780
YutoAraki <mjgda85200@yahoo.co.jp>
2019-09-15 14:41:53 +0900

    販売手数料と販売利益を表示をJSに変更

---
+++ app/assets/javascripts/price.js
@@ -0,0 +1,15 @@
+$(document).on('turbolinks:load', function(){
+  $('#price').on('keyup', function() {
+    let input = $('#price').val();
+    let price = Number(input);
+    if (typeof price == 'number' && 9999999 >= price && price >= 300){
+    let comissionFee = Math.floor(price / 10);
+    let profit = price - comissionFee;
+    $('.buy__container__main__wrapper__price__select__fee__right > p').text('¥' + comissionFee.toLocaleString());
+    $('.buy__container__main__wrapper__price__select__profit__right > p').text('¥' + profit.toLocaleString());
+  } else {
+    $('.buy__container__main__wrapper__price__select__fee__right > p').text('-');
+    $('.buy__container__main__wrapper__price__select__profit__right > p').text('-');
+  }
+  })
+});
