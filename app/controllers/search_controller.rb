979a42a0834b9c9c256225f03801768b50adc171
miyuki-muneishi <kyoruni@gmail.com>
2019-09-16 09:56:44 +0900

    #100 エラー修正

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
 class SearchController < ApplicationController
   before_action :set_category_list, only: [:index]
+  before_action :set_brand_list,    only: [:index]
   def index
     # 検索ワードが入力されていたら、検索
     @items     = Item.where('name LIKE(?)', "%#{params[:keyword]}%").order("created_at DESC") unless params[:keyword].empty?
